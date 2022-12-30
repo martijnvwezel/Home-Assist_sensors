92e7fbfd1badc3e18edc30e6eca7eab09bd64912
martijn <martijnvwezel@muino.nl>
2022-12-30 21:29:09 +0100

    Adding JPEG encoder

--- esp-heatcam/esp-heatcam.ino
+++ esp-heatcam/esp-heatcam.ino
@@ -1,33 +1,26 @@
-// #ifdef ESP32
-
+#ifdef ESP32
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
-// #elif defined(ESP8266)
-// #include <ESP8266WiFi.h>
-// #include <WiFiClient.h>
-// #include <ESP8266WebServer.h>
-// #include <ESP8266mDNS.h>
-// #endif
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#endif
 
 #include <Adafruit_MLX90640.h>
-// #include "esp_camera.h"
-// #include <WiFi.h>
-#include "esp_timer.h"
 
 #include "settings.h"
 #include "Arduino.h"
 #include "fb_gfx.h"
 #include "soc/soc.h"          //disable brownout problems
-#include "soc/rtc_cntl_reg.h" //disable brownout problems
-#include "esp_http_server.h"
+#include <soc/rtc_cntl_reg.h> //disable brownout problems
+#include <esp_http_server.h>
 
-
-#define CONFIG_SPIRAM_SUPPORT 1
-#define CONFIG_SPIRAM_USE_MALLOC 1
-#include "img_converters.h"
-
+// #include "conversions/img_converter_blieb.h"
+// #include <img_converter.h>
 
 Adafruit_MLX90640 mlx;
 camera_fb_t       fb;
@@ -39,30 +32,36 @@ float             frame[32 * 24]; // buffer for full frame of temperatures
 // high range of the sensor (this will be red on the screen)
 #define MAXTEMP (float)35
 
-uint16_t displayPixelWidth, displayPixelHeight;
 #define PART_BOUNDARY "123456789000000000000987654321"
 static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
 static const char* _STREAM_BOUNDARY     = "\r\n--" PART_BOUNDARY "\r\n";
 static const char* _STREAM_PART         = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
 httpd_handle_t stream_httpd = NULL;
-
-bool fmt_2_jpg(uint8_t *src, size_t src_len, uint16_t width, uint16_t height, pixformat_t format, uint8_t quality, uint8_t ** out, size_t * out_len) {
-    int jpg_buf_len = 128*1024;
-    uint8_t * jpg_buf = (uint8_t *) malloc(jpg_buf_len);
-    if(!jpg_buf){
-        return NULL;
+static uint    sent         = 0;
+bool           fmt_2_jpg(uint8_t* src, size_t src_len, uint16_t width, uint16_t height, pixformat_t format, uint8_t quality, uint8_t** out, size_t* out_len) {
+    int      jpg_buf_len = 64 * 1024;
+    uint8_t* jpg_buf     = (uint8_t*)malloc(jpg_buf_len);
+    if (jpg_buf == NULL) {
+        return false;
     }
 
-    convert_image(src, width, height, format, quality, jpg_buf);
+    Serial.println("Succesfull malloc..");
+    memory_stream dst_stream(jpg_buf, jpg_buf_len);
+
+    if (!convert_image(src, width, height, format, quality, &dst_stream)) {
+        free(jpg_buf);
+        return false;
+    }
+
+    *out     = jpg_buf;
+    *out_len = dst_stream.get_size();
+    return true;
 }
 
-
-bool frame_2_jpg(camera_fb_t * fb, uint8_t quality, uint8_t ** out, size_t * out_len){
+bool frame_2_jpg(camera_fb_t* fb, uint8_t quality, uint8_t** out, size_t* out_len) {
     return fmt_2_jpg(fb->buf, fb->len, fb->width, fb->height, fb->format, quality, out, out_len);
 }
-
-
 
 static esp_err_t stream_handler(httpd_req_t* req) {
     // camera_fb_t *fb = NULL;
@@ -73,23 +72,28 @@ static esp_err_t stream_handler(httpd_req_t* req) {
 
     res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
     if (res != ESP_OK) {
-        Serial.print("1Res esp: "); Serial.println(res);
+        Serial.print("1Res esp: ");
+        Serial.println(res);
         return res;
     }
-
+    Serial.println("Start While loop");
     while (true) {
-        Serial.println("~");
+        Serial.print("foto: ");
+        Serial.println(sent++);
         if (mlx.getFrame(frame) != 0) {
             Serial.println("Failed");
             continue;
+        } else {
+            Serial.println("Got a frame..");
         }
 
+        Serial.println("Foto:");
         int colorTemp;
         for (uint8_t h = 0; h < 24; h++) {
             for (uint8_t w = 0; w < 32; w++) {
                 float t = frame[h * 32 + w];
-                // Serial.print(t, 1);
-                // Serial.print(", ");
+                Serial.print(t, 1);
+                Serial.print(", ");
 
                 t = min(t, MAXTEMP);
                 t = max(t, MINTEMP);
@@ -97,42 +101,47 @@ static esp_err_t stream_handler(httpd_req_t* req) {
                 uint8_t colorIndex = map(t, MINTEMP, MAXTEMP, 0, 255);
                 colorIndex         = constrain(colorIndex, 0, 255);
 
-                fb.buf[h * 32 + w] = (uint8_t)camColors[colorIndex];
+                fb.buf[h * 32 + w] = (uint8_t)frame[h * 32 + w]; // camColors[colorIndex];
             }
         }
+        Serial.println("");
 
-        // bool jpeg_converted = frame_2_jpg(&fb, 80, &_jpg_buf, &_jpg_buf_len);
+        bool jpeg_converted = frame_2_jpg(&fb, 80, &_jpg_buf, &_jpg_buf_len);
 
-        // void * bliem = malloc(64);
-        // if(bliem) {
-        //     Serial.println("malloc went success: ");
-        // }
-        bool jpeg_converted = true;
-        _jpg_buf = fb.buf;
-        _jpg_buf_len = fb.len;
+        if (!jpeg_converted) {
+            Serial.println("JPEG compression failed");
+            res = ESP_FAIL;
+        } else {
+            Serial.println("JPEG compression success");
+        }
+
         if (res == ESP_OK) {
             size_t hlen = snprintf((char*)part_buf, 64, _STREAM_PART, _jpg_buf_len);
             res         = httpd_resp_send_chunk(req, (const char*)part_buf, hlen);
-            Serial.print("2Res esp: "); Serial.println(res);
+            Serial.print("2Res esp: ");
+            Serial.println(res);
         }
         if (res == ESP_OK) {
             res = httpd_resp_send_chunk(req, (const char*)_jpg_buf, _jpg_buf_len);
-            Serial.print("3Res esp: "); Serial.println(res);
+            Serial.print("3Res esp: ");
+            Serial.println(res);
         }
         if (res == ESP_OK) {
             res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-            Serial.print("4Res esp: "); Serial.println(res);
+            Serial.print("4Res esp: ");
+            Serial.println(res);
         }
 
-        // _jpg_buf = NULL;
+        _jpg_buf = NULL;
 
-        // if (_jpg_buf) {
-        //     free(_jpg_buf);
-        //     _jpg_buf = NULL;
-        // }
-        // if (res != ESP_OK) {
-        //     break;
-        // }
+        if (_jpg_buf) {
+            free(_jpg_buf);
+            _jpg_buf = NULL;
+        }
+        if (res != ESP_OK) {
+            break;
+        }
+        Serial.printf("MJPG: %uB\n", (uint32_t)(_jpg_buf_len));
     }
     return res;
 }
@@ -152,21 +161,24 @@ void startCameraServer() {
 void setup() {
 
     Serial.begin(115200);
-    Serial.print("Total heap: ");Serial.println(ESP.getHeapSize());
-    Serial.print("Free heap: ");Serial.println(ESP.getFreeHeap());
-    Serial.print("Total PSRAM: ");Serial.println(ESP.getPsramSize());
-    Serial.print("Free PSRAM: ");Serial.println(ESP.getFreePsram());
+    Serial.print("Total heap: ");
+    Serial.println(ESP.getHeapSize());
+    Serial.print("Free heap: ");
+    Serial.println(ESP.getFreeHeap());
+    Serial.print("Total PSRAM: ");
+    Serial.println(ESP.getPsramSize());
+    Serial.print("Free PSRAM: ");
+    Serial.println(ESP.getFreePsram());
     // uint8_t * jpg_buf = (uint8_t *)malloc(32*24);
     // Serial.println(jpg_buf);
 
     uint8_t buffer[32 * 24];
-    fb.buf               = buffer;              /*!< Pointer to the pixel data */
-    fb.len               = 24 * 32;             /*!< Length of the buffer in bytes */
-    fb.width             = 32;                  /*!< Width of the buffer in pixels */
-    fb.height            = 24;                  /*!< Height of the buffer in pixels */
-    fb.format            = PIXFORMAT_GRAYSCALE; /*!< Format of the pixel data */
+    fb.buf    = buffer;              /*!< Pointer to the pixel data */
+    fb.len    = 24 * 32;             /*!< Length of the buffer in bytes */
+    fb.width  = 32;                  /*!< Width of the buffer in pixels */
+    fb.height = 24;                  /*!< Height of the buffer in pixels */
+    fb.format = PIXFORMAT_GRAYSCALE; /*!< Format of the pixel data */
     // fb.timeval           = 0;                   /*!< Timestamp since boot of the first DMA buffer of the frame */
-
 
     delay(100);
 
--- esp-heatcam/settings.h
+++ esp-heatcam/settings.h
@@ -1,32 +1,65 @@
-#include <malloc.h>
-#include <stddef.h>
-#include <string.h>
-#include "esp_attr.h"
-#include "soc/efuse_reg.h"
-#include "esp_heap_caps.h"
-#include "esp_camera.h"
-#include "img_converters.h"
-#include "jpge.h"
-#include "yuv.h"
+#include "jpge_.h"
 
-// typedef struct
-// {
-//     uint8_t *buf;                   /*!< Pointer to the pixel data */
-//     size_t len = 24 * 32;           /*!< Length of the buffer in bytes */
-//     size_t width = 32;              /*!< Width of the buffer in pixels */
-//     size_t height = 24;             /*!< Height of the buffer in pixels */
-//     pixformat_t format = PIXFORMAT_GRAYSCALE; /*!< Format of the pixel data */
-//     struct timeval timestamp;       /*!< Timestamp since boot of the first DMA buffer of the frame */
-// } camera_fb_t_fake;
+typedef enum {
+    PIXFORMAT_RGB565,    // 2BPP/RGB565
+    PIXFORMAT_YUV422,    // 2BPP/YUV422
+    PIXFORMAT_GRAYSCALE, // 1BPP/GRAYSCALE
+    PIXFORMAT_JPEG,      // JPEG/COMPRESSED
+    PIXFORMAT_RGB888,    // 3BPP/RGB888
+    PIXFORMAT_RAW,       // RAW
+    PIXFORMAT_RGB444,    // 3BP2P/RGB444
+    PIXFORMAT_RGB555,    // 3BP2P/RGB555
+} pixformat_t;
 
-// camera_fb_t *fb = camera_fb_t_fake;
+typedef struct {
+    uint8_t * buf;              /*!< Pointer to the pixel data */
+    size_t len;                 /*!< Length of the buffer in bytes */
+    size_t width;               /*!< Width of the buffer in pixels */
+    size_t height;              /*!< Height of the buffer in pixels */
+    pixformat_t format;         /*!< Format of the pixel data */
+    struct timeval timestamp;   /*!< Timestamp since boot of the first DMA buffer of the frame */
+} camera_fb_t;
 
-// the colors we will be using
+
 // the colors we will be using
 const uint16_t camColors[] = {
     0x480F, 0x400F, 0x400F, 0x400F, 0x4010, 0x3810, 0x3810, 0x3810, 0x3810, 0x3010, 0x3010, 0x3010, 0x2810, 0x2810, 0x2810, 0x2810, 0x2010, 0x2010, 0x2010, 0x1810, 0x1810, 0x1811, 0x1811, 0x1011, 0x1011, 0x1011, 0x0811, 0x0811, 0x0811, 0x0011, 0x0011, 0x0011, 0x0011, 0x0011, 0x0031, 0x0031, 0x0051, 0x0072, 0x0072, 0x0092, 0x00B2, 0x00B2, 0x00D2, 0x00F2, 0x00F2, 0x0112, 0x0132, 0x0152, 0x0152, 0x0172, 0x0192, 0x0192, 0x01B2, 0x01D2, 0x01F3, 0x01F3, 0x0213, 0x0233, 0x0253, 0x0253, 0x0273, 0x0293, 0x02B3, 0x02D3, 0x02D3, 0x02F3, 0x0313, 0x0333, 0x0333, 0x0353, 0x0373, 0x0394, 0x03B4, 0x03D4, 0x03D4, 0x03F4, 0x0414, 0x0434, 0x0454, 0x0474, 0x0474, 0x0494, 0x04B4, 0x04D4, 0x04F4, 0x0514,
     0x0534, 0x0534, 0x0554, 0x0554, 0x0574, 0x0574, 0x0573, 0x0573, 0x0573, 0x0572, 0x0572, 0x0572, 0x0571, 0x0591, 0x0591, 0x0590, 0x0590, 0x058F, 0x058F, 0x058F, 0x058E, 0x05AE, 0x05AE, 0x05AD, 0x05AD, 0x05AD, 0x05AC, 0x05AC, 0x05AB, 0x05CB, 0x05CB, 0x05CA, 0x05CA, 0x05CA, 0x05C9, 0x05C9, 0x05C8, 0x05E8, 0x05E8, 0x05E7, 0x05E7, 0x05E6, 0x05E6, 0x05E6, 0x05E5, 0x05E5, 0x0604, 0x0604, 0x0604, 0x0603, 0x0603, 0x0602, 0x0602, 0x0601, 0x0621, 0x0621, 0x0620, 0x0620, 0x0620, 0x0620, 0x0E20, 0x0E20, 0x0E40, 0x1640, 0x1640, 0x1E40, 0x1E40, 0x2640, 0x2640, 0x2E40, 0x2E60, 0x3660, 0x3660, 0x3E60, 0x3E60, 0x3E60, 0x4660, 0x4660, 0x4E60, 0x4E80, 0x5680, 0x5680, 0x5E80, 0x5E80, 0x6680, 0x6680,
     0x6E80, 0x6EA0, 0x76A0, 0x76A0, 0x7EA0, 0x7EA0, 0x86A0, 0x86A0, 0x8EA0, 0x8EC0, 0x96C0, 0x96C0, 0x9EC0, 0x9EC0, 0xA6C0, 0xAEC0, 0xAEC0, 0xB6E0, 0xB6E0, 0xBEE0, 0xBEE0, 0xC6E0, 0xC6E0, 0xCEE0, 0xCEE0, 0xD6E0, 0xD700, 0xDF00, 0xDEE0, 0xDEC0, 0xDEA0, 0xDE80, 0xDE80, 0xE660, 0xE640, 0xE620, 0xE600, 0xE5E0, 0xE5C0, 0xE5A0, 0xE580, 0xE560, 0xE540, 0xE520, 0xE500, 0xE4E0, 0xE4C0, 0xE4A0, 0xE480, 0xE460, 0xEC40, 0xEC20, 0xEC00, 0xEBE0, 0xEBC0, 0xEBA0, 0xEB80, 0xEB60, 0xEB40, 0xEB20, 0xEB00, 0xEAE0, 0xEAC0, 0xEAA0, 0xEA80, 0xEA60, 0xEA40, 0xF220, 0xF200, 0xF1E0, 0xF1C0, 0xF1A0, 0xF180, 0xF160, 0xF140, 0xF100, 0xF0E0, 0xF0C0, 0xF0A0, 0xF080, 0xF060, 0xF040, 0xF020, 0xF800,
+};
+
+
+class memory_stream : public jpge::output_stream {
+protected:
+    uint8_t *out_buf;
+    size_t max_len, index;
+
+public:
+    memory_stream(void *pBuf, uint buf_size) : out_buf(static_cast<uint8_t*>(pBuf)), max_len(buf_size), index(0) { }
+
+    virtual ~memory_stream() { }
+
+    virtual bool put_buf(const void* pBuf, int len)
+    {
+        if (!pBuf) {
+            //end of image
+            return true;
+        }
+        if ((size_t)len > (max_len - index)) {
+            //ESP_LOGW(TAG, "JPG output overflow: %d bytes (%d,%d,%d)", len - (max_len - index), len, index, max_len);
+            len = max_len - index;
+        }
+        if (len) {
+            memcpy(out_buf + index, pBuf, len);
+            index += len;
+        }
+        return true;
+    }
+
+    virtual size_t get_size() const
+    {
+        return index;
+    }
 };
 
 const char* ssid     = "ssid";
@@ -52,32 +85,11 @@ static IRAM_ATTR void convert_line_format(uint8_t* src, pixformat_t format, uint
             dst[o++] = (src[i] & 0x07) << 5 | (src[i + 1] & 0xE0) >> 3;
             dst[o++] = (src[i + 1] & 0x1F) << 3;
         }
-    } else if (format == PIXFORMAT_YUV422) {
-        uint8_t y0, y1, u, v;
-        uint8_t r, g, b;
-        l = width * 2;
-        src += l * line;
-        for (i = 0; i < l; i += 4) {
-            y0 = src[i];
-            u  = src[i + 1];
-            y1 = src[i + 2];
-            v  = src[i + 3];
-
-            yuv2rgb(y0, u, v, &r, &g, &b);
-            dst[o++] = r;
-            dst[o++] = g;
-            dst[o++] = b;
-
-            yuv2rgb(y1, u, v, &r, &g, &b);
-            dst[o++] = r;
-            dst[o++] = g;
-            dst[o++] = b;
-        }
     }
 }
 
 
-bool convert_image(uint8_t *src, uint16_t width, uint16_t height, pixformat_t format, uint8_t quality, uint8_t *dst_stream)
+bool convert_image(uint8_t *src, uint16_t width, uint16_t height, pixformat_t format, uint8_t quality, jpge::output_stream *dst_stream)
 {
     int num_channels = 3;
     jpge::subsampling_t subsampling = jpge::H2V2;
@@ -100,20 +112,20 @@ bool convert_image(uint8_t *src, uint16_t width, uint16_t height, pixformat_t fo
     jpge::jpeg_encoder dst_image;
 
     if (!dst_image.init(dst_stream, width, height, num_channels, comp_params)) {
-        // Serial.print(TAG, "JPG encoder init failed");
+        Serial.print( "JPG encoder init failed");
         return false;
     }
 
     uint8_t* line = (uint8_t*)malloc(width * num_channels);
     if(!line) {
-        // Serial.print(TAG, "Scan line malloc failed");
+        Serial.print( "Scan line malloc failed");
         return false;
     }
 
     for (int i = 0; i < height; i++) {
         convert_line_format(src, format, line, width, num_channels, i);
         if (!dst_image.process_scanline(line)) {
-            // Serial.print(TAG, "JPG process line %u failed", i);
+            Serial.print("JPG process line failed: ");Serial.println(i);
             free(line);
             return false;
         }
@@ -121,7 +133,7 @@ bool convert_image(uint8_t *src, uint16_t width, uint16_t height, pixformat_t fo
     free(line);
 
     if (!dst_image.process_scanline(NULL)) {
-        // Serial.print(TAG, "JPG image finish failed");
+        Serial.println("JPG image finish failed");
         return false;
     }
     dst_image.deinit();
