172cc1a278fe4499c73092e710dfb7f9764a53e8
martijn <martijnvwezel@muino.nl>
2023-05-01 18:08:38 +0200

    Implement working version!

--- Watersensor/src/settings.h
+++ Watersensor/src/settings.h
@@ -3,7 +3,6 @@
 // *************************************************
 // * Configurble settings, change if needed
 // *************************************************
-
 const char* ssid     = "cc";
 const char* password = "ccc";
 
@@ -26,6 +25,33 @@ const int   mqtt_port     = 1883;
 #define LED D6
 #define LIGHT_SEN_ENABLE D5
 
+uint32_t mili_liters_total = 0; // * since boot
+uint32_t old_sensor_value = 0;
+bool under_value = false;
+
+
+static float mina = 3000, minb = 3000, minc = 3000;
+
+#define BUFFER_SIZE 256
+struct data_buffer {
+    uint32_t sens_a[BUFFER_SIZE];
+    uint32_t sen_a_pos = 0;
+
+    uint32_t sens_b[BUFFER_SIZE];
+    uint32_t sen_b_pos = 0;
+
+    uint32_t sens_c[BUFFER_SIZE];
+    uint32_t sen_c_pos = 0;
+};
+
+
+
+#define PI_3 1.0471975512
+#define PI2_3 2.09439510239
+#define PI2 6.28318530718
+#define PI  3.14159265358
+
+
 
 // * The hostname of our little creature
 #define HOSTNAME "MuinoWaterMeter"
@@ -38,10 +64,6 @@ long LAST_RECONNECT_ATTEMPT = 0;
 
 // * MQTT root topic
 #define MQTT_ROOT_TOPIC "MuinoWaterMeter"
-
-uint32_t mili_liters_total = 0; // * since boot
-uint32_t old_sensor_value = 0;
-bool under_value = false;
 
 
 // * Initiate WIFI client
@@ -92,8 +114,8 @@ bool mqtt_reconnect() {
 
 // * Send a message to a broker topic
 void send_mqtt_message(const char* topic, char* payload) {
-    Serial.printf("MQTT Outgoing on %s: ", topic);
-    Serial.println(payload);
+    // Serial.printf("MQTT Outgoing on %s: ", topic);
+    // Serial.println(payload);
 
     bool result = mqtt_client.publish(topic, payload, false);
 
@@ -103,6 +125,36 @@ void send_mqtt_message(const char* topic, char* payload) {
 }
 
 
+void swap(float* xp, float* yp) {
+    float temp = *xp;
+    *xp        = *yp;
+    *yp        = temp;
+}
+
+// A function to implement bubble sort
+void bubbleSort(float arr[], int n) {
+    for (int i = 0; i < n - 1; i++)
+
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+}
+
+float median(float array[], size_t length) {
+
+    bubbleSort(array, length);
+
+    if (length == 0) {
+        printf("no median for empty data\n");
+    }
+    if (length % 2 == 1) {
+        return array[length / 2];
+    } else {
+        int i = length / 2;
+        return (array[i - 1] + array[i]) / 2;
+    }
+}
 
 
 
@@ -111,10 +163,10 @@ void send_mqtt_message(const char* topic, char* payload) {
 
 
 void send_metric(String name, uint32_t metric) {
-    Serial.print(F("Sending metric to broker: "));
-    Serial.print(name);
-    Serial.print(F("="));
-    Serial.println(metric);
+    // Serial.print(F("Sending metric to broker: "));
+    // Serial.print(name);
+    // Serial.print(F("="));
+    // Serial.println(metric);
 
     char output[10];
     ltoa(metric, output, sizeof(output));
@@ -141,3 +193,55 @@ void send_data_to_broker() {
     // send_metric("pulse", 500);
     send_metric("total_after_boot", mili_liters_total);
 }
+
+
+// void update_minima(uint32_t sen_a, uint32_t sen_b, uint32_t sen_c) {
+//     // Find minima and average
+//     static uint32_t average_a = 0 , average_b = 0 , average_c = 0 ;
+//     raw_data_buffer.sens_a[raw_data_buffer.sen_a_pos++] = sen_a;
+//     raw_data_buffer.sens_b[raw_data_buffer.sen_b_pos++] = sen_b;
+//     raw_data_buffer.sens_c[raw_data_buffer.sen_c_pos++] = sen_c;
+
+//     if (raw_data_buffer.sen_a_pos >= BUFFER_SIZE) {
+//         uint32_t cnt_a = 0, cnt_b = 0, cnt_c = 0;
+//         for (size_t i = 0; i < BUFFER_SIZE; i++) {
+//             if (raw_data_buffer.sens_a[i] > 10) {
+//                 mina = MIN(mina, raw_data_buffer.sens_a[i]); // TODO if new minima is found check how many times that happend
+//             }
+//             if (raw_data_buffer.sens_b[i] > 10) {
+//                 minb = MIN(minb, raw_data_buffer.sens_b[i]);
+//             }
+//             if (raw_data_buffer.sens_c[i] > 10) {
+//                 minc = MIN(minc, raw_data_buffer.sens_c[i]);
+//             }
+
+//             cnt_a = cnt_a + raw_data_buffer.sens_a[i];
+//             cnt_b = cnt_b + raw_data_buffer.sens_b[i];
+//             cnt_c = cnt_c + raw_data_buffer.sens_c[i];
+//         }
+
+//         average_a = cnt_a = cnt_a / (BUFFER_SIZE);
+//         average_b = cnt_b = cnt_b / (BUFFER_SIZE);
+//         average_c = cnt_c = cnt_c / (BUFFER_SIZE);
+
+//         raw_data_buffer.sen_a_pos = 0;
+//         raw_data_buffer.sen_b_pos = 0;
+//         raw_data_buffer.sen_c_pos = 0;
+//     }
+// }
+
+// size_t minima_pos = 0;
+// bool full_minim_arr = false;
+// float minima_arr [32];
+// float minimb_arr [32];
+// float minimc_arr [32];
+
+// float minima_value(float *minim_arr, float new_value){
+//     minim_arr[minima_pos++] = new_value;
+//     if (minima_pos >= 32) {
+//         full_minim_arr = true;
+//         minima_pos     = 0;
+//     }
+//     float minimc_arr[32];
+//     memcpy(minimc_arr, minim_arr, minima_pos);
+// }
--- Watersensor/src/water-meter.ino
+++ Watersensor/src/water-meter.ino
@@ -22,66 +22,113 @@
 #include <math.h>
 
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
-#define PI_3 1.0471975512
+// #define MIN_AVERAGE(X, Y) (((X+10) <= (Y) && (Y) > 10 ) ? (X) : ((Y+X)/2))
+struct data_buffer raw_data_buffer;
 
-float phase_iter(float a, float b, float c){
-    static float mina = 442, minb = 1140, minc = 312;
-    static float phase = 1;
-    mina = MIN(mina,a);
-    minb = MIN(minb,b);
-    minc = MIN(minc,c);
-
-    const float epsilon = 0.1;
-    float less = (a-mina)*pow(sin(phase-epsilon),2)+(b-minb)*pow(sin(phase-epsilon+PI_3),2)+(c-minc)*pow(sin(phase-epsilon-PI_3),2);
-    float same = (a-mina)*pow(sin(phase),2)+(b-minb)*pow(sin(phase+PI_3),2)+(c-minc)*pow(sin(phase-PI_3),2);
-    float more = (a-mina)*pow(sin(phase+epsilon),2)+(b-minb)*pow(sin(phase+epsilon+PI_3),2)+(c-minc)*pow(sin(phase+epsilon-PI_3),2);
-    if(more>same && more>less)
-        phase += 0.1;
-    if(less>same && less>more)
-        phase -= 0.1;
-    return phase;
+#define ALPHA_COR 0.1 // Value between 0-1
+float mini_average(float x, float y) {
+    if ((x + 10) <= y && y > 10) {
+        return x;
+    } else {
+        return (1 - ALPHA_COR) * x + ALPHA_COR * y;
+    }
 }
 
-int magic_code_box(uint32_t sen_a, uint32_t sen_b, uint32_t sen_c) {
+// float phase_iter(float a, float b, float c, float mina, float minb, float minc, float epsilon, float amplitude) {
+//     static float phase = 0;
+//     float        less  = (a + amplitude - mina) * sin(phase - epsilon) + (b + amplitude - minb) * sin(phase - epsilon + PI2_3) + (c + amplitude - minc) * sin(phase - epsilon - PI2_3);
+//     float        same  = (a + amplitude - mina) * sin(phase) + (b + amplitude - minb) * sin(phase + PI2_3) + (c + amplitude - minc) * sin(phase - PI2_3);
+//     float        more  = (a + amplitude - mina) * sin(phase + epsilon) + (b + amplitude - minb) * sin(phase + epsilon + PI2_3) + (c + amplitude - minc) * sin(phase + epsilon - PI2_3);
+//     if (more > same && more > less)
+//         phase += epsilon;
+//     if (less > same && less > more)
+//         phase -= epsilon;
+//     return phase;
+// }
+
+float phase_iter(float phase, float a, float b, float c) {
     // * Liter berekening
+    // * Amplitude voor evenwichtspunt
     // * asin^2(σ)+bsin^2(σ+π/3)+c*sin^3(σ-π/3)
     // * a⋅sin²(σ±ε)+b⋅sin²(σ±ε+π/3)+c⋅sin²(σ±ε-π/3)
-    bool temp = under_value;
+
+    float sin1 = sin(phase);
+    float sin2 = sin(phase + PI2_3);
+    float sin3 = -sin2; // sin(phase + PI4_3);
+    float less = a * sin3 + b * sin1 + c * sin2;
+    float same = a * sin1 + b * sin2 + c * sin3;
+    float more = a * sin2 + b * sin3 + c * sin1;
+    if (more > same && more >= less)
+        return phase + PI_3;
+    else if (less > same)
+        return phase - PI_3;
+    else
+        return phase;
+}
+
+int magic_code_box(float sen_a, float sen_b, float sen_c) {
+    static float prev_a = 0, prev_b = 0, prev_c = 0;
+    static float phase = 0;
+
+    // * Calculate minimum value
+    mina = mini_average(mina, sen_a);
+    minb = mini_average(minb, sen_b);
+    minc = mini_average(minc, sen_c);
+
+    // * Add minima + amplitude to signal to get zerocrossing
+    float amplitude = 50.0;
+    float a = sen_a - mina + amplitude;
+    float b = sen_b - minb + amplitude;
+    float c = sen_c - minc + amplitude;
 
 
-    float phase = phase_iter((float)sen_a,(float)sen_b,(float)sen_c);
 
-    uint32_t mili_liters = (uint32_t)((phase*1000)/PI_3);
-    old_sensor_value = mili_liters_total;
-    mili_liters_total = mili_liters_total + mili_liters;
 
-    if ((old_sensor_value+1000) > mili_liters_total){
-        return 0;
-    }
+
+    // * Get amount of liters
+    phase = phase_iter(phase, a, b, c);
+    // float    phase       = phase_iter(sen_a, sen_b, sen_c,  mina, minb, minc, 0.5, 50.0);
+    uint32_t mili_liters = ((uint32_t)((phase * 100) / PI2)) * 10;
+    mili_liters_total    = mili_liters;
+
+    prev_a = sen_a;
+    prev_b = sen_b;
+    prev_c = sen_c;
+
+    // if ((old_sensor_value+1000) > mili_liters_total) {
+    //     old_sensor_value = mili_liters_total;
+    //     return 1;
+    // }
 
     return 1;
 }
 
-
-void do_water_measurment(){
+void do_water_measurment() {
 
     digitalWrite(LED, HIGH);
-    delay(20);
+    delay(5);
     uint32_t sen_a = analogReadMilliVolts(SENS_A);
     uint32_t sen_b = analogReadMilliVolts(SENS_B);
     uint32_t sen_c = analogReadMilliVolts(SENS_C);
     digitalWrite(LED, LOW);
 
+    // Serial.print(sen_a);
+    // Serial.print("  ");
+    // Serial.print(sen_b);
+    // Serial.print("  ");
+    // Serial.print(sen_c);
+    // Serial.print("  ");
+    // Serial.print(mina);
+    // Serial.print("  ");
+    // Serial.print(minb);
+    // Serial.print("  ");
+    // Serial.print(minc);
+    // Serial.print("  ");
+    // Serial.println(mili_liters_total);
 
-    Serial.print(sen_a, 6);
-    Serial.print("  ");
-    Serial.print(sen_b, 6);
-    Serial.print("  ");
-    Serial.println(sen_c, 6);
-
-    if (magic_code_box(sen_a, sen_b, sen_c))
+    bool send = magic_code_box((float)sen_a, (float)sen_b, (float)sen_c);
+    if (send)
         send_data_to_broker();
-
 
     delay(80);
 }
@@ -89,8 +136,7 @@ void do_water_measurment(){
 void setup() {
 
     Serial.begin(115200);
-    Serial.print("Water sensor V1.0.0!");
-
+    Serial.print("Water sensor V1.0.3!");
 
     // Wi-Fi connection
     Serial.println("Connecting to " + String(ssid) + "..");
@@ -99,7 +145,7 @@ void setup() {
     WiFi.mode(WIFI_STA);
 
     WiFi.begin(ssid, password);
-   int deadCounter = 20;
+    int deadCounter = 20;
     while (WiFi.status() != WL_CONNECTED && deadCounter-- > 0) {
         delay(500);
         Serial.print(".");
@@ -117,8 +163,7 @@ void setup() {
         Serial.println(WiFi.localIP());
     }
 
-
-   // Port defaults to 8266
+    // Port defaults to 8266
     // ArduinoOTA.setPort(8266);
 
     // Hostname defaults to esp8266-[ChipID]
@@ -155,23 +200,12 @@ void setup() {
     // * Setup MQTT
     Serial.printf("MQTT connecting to: %s:%d\n", mqtt_server, mqtt_port);
     mqtt_client.setServer(mqtt_server, mqtt_port);
-
-
-
 }
 
 void loop() {
 
-    ArduinoOTA.handle();
-
-
-
-
-
-
+    // ArduinoOTA.handle();
     do_water_measurment();
-
-
 }
 
 // 1132 044 3352
